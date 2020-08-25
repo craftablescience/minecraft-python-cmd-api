71629751daae826c20614d8be711b9666f3ee5bd
Brendan Lewis <brendanelewis@outlook.com>
2020-08-25 11:23:45 -0400

    Feat: run Python commands from game

--- src/main/java/io/github/craftablescience/pythoncmdapi/command/RunPythonFileCommand.java
+++ src/main/java/io/github/craftablescience/pythoncmdapi/command/RunPythonFileCommand.java
@@ -10,9 +10,11 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Style;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
-import org.python.util.PythonInterpreter;
 
 import java.io.File;
+
+import static io.github.craftablescience.pythoncmdapi.PythonCommandAPI.MOD_ID;
+import static io.github.craftablescience.pythoncmdapi.PythonCommandAPI.PY_INTERP;
 
 
 public class RunPythonFileCommand {
@@ -23,22 +25,40 @@ public class RunPythonFileCommand {
                 .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
                 .then(CommandManager.argument("filename", StringReader::readString)
                 .executes(RunPythonFileCommand::python))));
+        CommandRegistrationCallback.EVENT.register((commandDispatcher, b) ->
+            commandDispatcher.register(LiteralArgumentBuilder.<ServerCommandSource>literal("py_runline")
+                .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
+                .then(CommandManager.argument("code to execute", StringReader::readString)
+                .executes(RunPythonFileCommand::py_runline))));
     }
 
     private static int python(CommandContext<ServerCommandSource> context) {
         context.getSource().getMinecraftServer().execute(() -> {
             String path = context.getArgument("filename", String.class).trim();
-            context.getSource().sendFeedback(new TranslatableText("pythoncmdapi.commands.python.loading", path).setStyle(Style.EMPTY.withColor(Formatting.GREEN)), true);
+            context.getSource().sendFeedback(new TranslatableText(MOD_ID + ".commands.python.loading", path).setStyle(Style.EMPTY.withColor(Formatting.GREEN)), true);
 
             try {
-                PythonInterpreter pi = new PythonInterpreter();
-                pi.execfile(PythonCommandAPI.PYTHON_DIR.toString() + File.separator + path);
+                PY_INTERP.execfile(PythonCommandAPI.PYTHON_DIR.toString() + File.separator + path);
             } catch (Exception e) {
-                context.getSource().sendError(new TranslatableText("pythoncmdapi.commands.python.failure", e.getMessage()).setStyle(Style.EMPTY.withColor(Formatting.RED)));
+                context.getSource().sendError(new TranslatableText(MOD_ID + ".commands.python.failure", e.getMessage()).setStyle(Style.EMPTY.withColor(Formatting.RED)));
                 e.printStackTrace();
                 return;
             }
-            context.getSource().sendFeedback(new TranslatableText("pythoncmdapi.commands.python.done", path).setStyle(Style.EMPTY.withColor(Formatting.GREEN)), true);
+            context.getSource().sendFeedback(new TranslatableText(MOD_ID + ".commands.python.done", path).setStyle(Style.EMPTY.withColor(Formatting.GREEN)), true);
+        });
+        return -1;
+    }
+
+    private static int py_runline(CommandContext<ServerCommandSource> context) {
+        context.getSource().getMinecraftServer().execute(() -> {
+            String code = context.getArgument("code to execute", String.class).trim();
+
+            try {
+                PY_INTERP.exec(code);
+            } catch (Exception e) {
+                context.getSource().sendError(new TranslatableText(MOD_ID + ".commands.py_runline.failure", code, e.getMessage()).setStyle(Style.EMPTY.withColor(Formatting.RED)));
+                e.printStackTrace();
+            }
         });
         return -1;
     }
